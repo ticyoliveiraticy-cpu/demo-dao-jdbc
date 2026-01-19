bc1415533938aa3052a51dbc2e8b531bbfbcecca
ticyoliveiraticy-cpu <ticyoliveiraticy@gmail.com>
2026-01-19 07:56:38 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Department;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void insert(SellerDao obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(SellerDao obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SellerDao> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
